79c9d39f497e9fab3f126c891e67952fb97b25db
kensiiwasaki <kensiiwasaki@gmail.com>
2023-11-21 15:38:23 +0900

    modify: サインアウト処理追加

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,15 +3,18 @@ import {
   SignedOut,
   RedirectToSignIn,
   UserButton,
+  useClerk,
 } from "@clerk/remix";
 
 export default function Index() {
+  const { signOut } = useClerk();
   return (
     <div>
       <SignedIn>
         <h1>Index route</h1>
         <p>You are signed in!</p>
         <UserButton />
+        <button onClick={() => signOut()}>サインアウト</button>
       </SignedIn>
       <SignedOut>
         <RedirectToSignIn />
