e867773f64358bf18473defeeeb5766134d8f638
kensiiwasaki <kensiiwasaki@gmail.com>
2023-11-21 12:43:10 +0900

    不要なコード削除

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
 import {
-  Form,
   Links,
   LiveReload,
   Meta,
@@ -17,33 +16,8 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
+        <div>
+          <h1>Remix</h1>
         </div>
 
         <ScrollRestoration />
