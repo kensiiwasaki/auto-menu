4427d0927086f1e8b873adb630031ce28e6e0161
kensiiwasaki <kensiiwasaki@gmail.com>
2023-11-21 14:55:52 +0900

    modify: clerkによるユーザー認証追加

--- app/root.tsx
+++ app/root.tsx
@@ -1,21 +1,42 @@
 import "@mantine/core/styles.css";
 
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import type {
+  LinksFunction,
+  LoaderFunction,
+  MetaFunction,
+} from "@remix-run/node";
 import {
   Links,
   LiveReload,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
+import { ClerkApp, ClerkErrorBoundary } from "@clerk/remix";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
-export default function App() {
+export const meta: MetaFunction = () => [
+  {
+    charset: "utf-8",
+    title: "New Remix App",
+    viewport: "width=device-width,initial-scale=1",
+  },
+];
+
+export const loader: LoaderFunction = (args) => {
+  return rootAuthLoader(args);
+};
+
+export const ErrorBoundary = ClerkErrorBoundary();
+
+function App() {
   return (
     <html lang="ja">
       <head>
@@ -31,6 +52,7 @@ export default function App() {
             <h1>Remix</h1>
           </div>
 
+          <Outlet />
           <ScrollRestoration />
           <Scripts />
           <LiveReload />
@@ -39,3 +61,5 @@ export default function App() {
     </html>
   );
 }
+
+export default ClerkApp(App);
