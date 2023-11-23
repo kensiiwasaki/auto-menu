c795620c9ce4d77e0ad525e73bb7113759264b6f
kensiiwasaki <kensiiwasaki@gmail.com>
2023-11-24 00:13:35 +0900

    modify: 不要なコメント削除

--- app/routes/example.tsx
+++ app/routes/example.tsx
@@ -3,7 +3,6 @@ import { useAuth } from "@clerk/remix";
 export default function Example() {
   const { isLoaded, userId, sessionId } = useAuth();
 
-  // In case the user signs out while on the page.
   if (!isLoaded || !userId) {
     return null;
   }
