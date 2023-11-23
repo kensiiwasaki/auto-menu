d88fb5ceeca0ceaba4b42bf65d1507118b8881c0
kensiiwasaki <kensiiwasaki@gmail.com>
2023-11-23 22:48:25 +0900

    modify: delete処理追加

--- app/datasource/prisma/task.ts
+++ app/datasource/prisma/task.ts
@@ -2,6 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const findAllTask = async (userId: string) => {
+export const findUserAllTask = async (userId: string) => {
   return await prisma.task.findMany({ where: { userId } });
+};
+
+export const deleteUserAllTask = async (userId: string) => {
+  return await prisma.task.deleteMany({
+    where: {
+      userId: userId,
+    },
+  });
 };
