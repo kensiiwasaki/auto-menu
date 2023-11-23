8d2e2e6b2db57a50979449a8e82f093d7a6adc2a
kensiiwasaki <kensiiwasaki@gmail.com>
2023-11-23 23:51:56 +0900

    modify: prismaの処理分割

--- app/datasource/prisma/task.ts
+++ app/datasource/prisma/task.ts
@@ -1,9 +1,21 @@
+import type { Task } from "@prisma/client";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export const findUserAllTask = async (userId: string) => {
   return await prisma.task.findMany({ where: { userId } });
+};
+
+export const findUniqueUserTask = async (taskId: number) => {
+  return await prisma.task.findUnique({ where: { id: Number(taskId) } });
+};
+
+export const updateUserTask = async (taskId: number, task: Task) => {
+  await prisma.task.update({
+    where: { id: taskId },
+    data: { completed: !task.completed },
+  });
 };
 
 export const deleteUserAllTask = async (userId: string) => {
